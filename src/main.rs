8f817430b26e5e041ffc03152a523fe65c2b1518
lukas <lukashermann28@gmail.com>
2023-02-26 17:30:39 -0800

    remove main file

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::io::Write;
-use tinystl::StlData;
-
-pub fn main() {
-    let args = std::env::args().collect::<Vec<String>>();
-    let data = StlData::read_from_file(&args[1]).unwrap();
-    let mut out = std::io::stdout().lock();
-    writeln!(&mut out, "{}", data.triangles.len()).unwrap();
-}
